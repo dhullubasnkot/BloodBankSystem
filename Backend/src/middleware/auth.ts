3badfb66dcf0e1f85b3c5003d7baec1fc408a9dd
dhullubasnkot <123baglung@gmail.com>
2025-06-21 10:30:33 +0545

    Updated Middle ware now  support different different Tokens

--- Backend/src/middleware/auth.ts
+++ Backend/src/middleware/auth.ts
@@ -5,15 +5,15 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
-const REFRESH_SECRET =
-  process.env.JWT_REFRESH_SECRET || "your_jwt_refresh_secret";
+const JWT_REFRESH_SECRET =
+  process.env.JWT_REFRESH_SECRET || "your_refresh_secret";
 
 export interface AuthRequest extends Request {
   user?: any;
 }
 
 function generateAccessToken(payload: object) {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1m" });
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "2m" });
 }
 
 export async function authenticateToken(
@@ -31,36 +31,45 @@ export async function authenticateToken(
   }
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET) as any;
+
+    if (decoded.type !== "access") throw new Error("Invalid access token");
+
     req.user = decoded;
     return next();
   } catch (error) {
     console.log("Access token expired or invalid:", (error as Error)?.message);
 
     try {
-      const decodedRefresh = jwt.verify(refreshToken, REFRESH_SECRET) as any;
+      const decodedRefresh = jwt.verify(
+        refreshToken,
+        JWT_REFRESH_SECRET
+      ) as any;
+
+      if (decodedRefresh.type !== "refresh")
+        throw new Error("Invalid refresh token type");
 
       const stored = await prisma.refreshToken.findUnique({
         where: { userId: decodedRefresh.id },
       });
 
       if (!stored || stored.rtoken !== refreshToken) {
-        res.status(401).json({ message: "Invalid refresh token" });
-        return;
+        return res.status(401).json({ message: "Invalid refresh token" });
       }
 
       const newAccessToken = generateAccessToken({
         id: decodedRefresh.id,
         name: decodedRefresh.name,
         email: decodedRefresh.email,
+        type: "access",
       });
 
       res.cookie("auth_token", newAccessToken, {
         httpOnly: true,
         secure: false,
         sameSite: "lax",
-        maxAge: 10 * 1000 ,
         path: "/",
+        maxAge: 2 * 60 * 1000, // 2 minutes
       });
 
       req.user = jwt.decode(newAccessToken);
