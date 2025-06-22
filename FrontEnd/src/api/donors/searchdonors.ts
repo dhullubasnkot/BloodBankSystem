3cc8b0d4e6352185ce06e5e31c792a8a08ae86d9
dhullubasnkot <123baglung@gmail.com>
2025-06-22 15:31:13 +0545

    Fetch Search APi

---
+++ FrontEnd/src/api/donors/searchdonors.ts
@@ -0,0 +1,20 @@
+const CreateDonors = async (formData) => {
+  const response = await fetch("http://localhost:3000/donor/search", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(formData),
+  });
+
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw new Error(result.message || "Failed to Search Donor");
+  }
+
+  return result;
+};
+
+export default CreateDonors;
