38e6fb921e3383deb324e1d3c3b733a4af3ebffd
dhullubasnkot <123baglung@gmail.com>
2025-06-26 10:40:31 +0545

    New update : FrontEnd/src/pages/ShowAllDonorAndRequester.jsx

---
+++ FrontEnd/src/pages/ShowAllDonorAndRequester.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import SidebarLayout from "./sidebarlayout";
+import FindBlood from "./findBloodByDonors";
+import RequestedBloodList from "./RequestedBloodList";
+import Navbar from "../components/navbar";
+
+const FindBloodDashboard = () => {
+  const [activeTab, setActiveTab] = useState("donors");
+
+  const renderTab = () => {
+    switch (activeTab) {
+      case "donors":
+        return <FindBlood />;
+      case "requests":
+        return <RequestedBloodList />;
+      default:
+        return <FindBlood />;
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex">
+        <SidebarLayout activeTab={activeTab} setActiveTab={setActiveTab} />
+        <div className="flex-1 p-4">{renderTab()}</div>
+      </div>
+    </>
+  );
+};
+
+export default FindBloodDashboard;
