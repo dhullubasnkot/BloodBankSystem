137de832f84adc5f2f8d8e8b1090d7de75e4c1fa
dhullubasnkot <123baglung@gmail.com>
2025-06-26 10:40:27 +0545

    New update : FrontEnd/src/api/donors/registerAsDonors.ts

--- FrontEnd/src/api/donors/registerAsDonors.ts
+++ FrontEnd/src/api/donors/registerAsDonors.ts
@@ -1,7 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
 const CreateDonors = async (formData) => {
-  const response = await fetch(`${API_URL}donor`, {
+  const response = await fetch("http://localhost:3000/donor", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
