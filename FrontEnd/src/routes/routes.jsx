6b56991923e788d94b1bde346eaf14d95dd3c9e3
dhullubasnkot <123baglung@gmail.com>
2025-06-26 10:40:29 +0545

    New update : FrontEnd/src/routes/routes.jsx

--- FrontEnd/src/routes/routes.jsx
+++ FrontEnd/src/routes/routes.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FindBlood from "../pages/findBlood";
+import FindBlood from "../pages/findBloodByDonors";
 import HeroSection from "../pages/hero";
 import OurCollobaration from "../pages/OurCollabration";
 import OurMission from "../pages/ourMission";
 import Navbar from "../components/navbar";
 import Register from "../auth/signup";
 import RegisterAsDonor from "../pages/RegisterAsDonor";
+import RequestBloodForm from "../pages/RequestBlood";
+import FindBloodDashboard from "../pages/ShowAllDonorAndRequester";
 
 const AppRoutes = () => (
   <>
     <Router>
       <Routes>
         <Route path="/" element={<HeroSection />} />
-        <Route path="/ourmission" element={<OurMission />} />
-        <Route path="/collaboration" element={<OurCollobaration />} />
+        <Route path="/DonorandRequetedBlood" element={<FindBloodDashboard />} />
         <Route path="/findblood" element={<FindBlood />} />
         <Route path="/BeaDonor" element={<RegisterAsDonor />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/RequestBlood" element={<RequestBloodForm />} />
       </Routes>
     </Router>
   </>
