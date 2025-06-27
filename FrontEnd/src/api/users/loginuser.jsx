609269553b51601929c65d54ddcb718ca3b3f8b0
dhullubasnkot <123baglung@gmail.com>
2025-06-27 13:00:54 +0545

    Fetch Valid Donor Id , and passed to localstorage if donor is already registred with login details

--- FrontEnd/src/api/users/loginuser.jsx
+++ FrontEnd/src/api/users/loginuser.jsx
@@ -1,24 +1,37 @@
 import { getOrCreateDeviceId } from "../../utils/deviceId";
 
 const LoginUser = async (formData) => {
-  const getDevicesId = getOrCreateDeviceId();
+  const deviceId = getOrCreateDeviceId();
+  const bodyData = {
+    ...formData,
+    deviceId,
+  };
+
   const response = await fetch("http://localhost:3000/user/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(formData, getDevicesId),
+    body: JSON.stringify(bodyData),
     credentials: "include",
   });
 
   const result = await response.json();
+
   if (!response.ok) {
-    throw new Error(result.message || "Signup failed");
+    throw new Error(result.message || "Login failed");
   }
+
   const user = result.user;
 
   localStorage.setItem("id", user.id);
   localStorage.setItem("userId", user.id);
   localStorage.setItem("username", user.name);
   localStorage.setItem("email", user.email);
+
+  if (result.donorId) {
+    localStorage.setItem("Donor_id", result.donorId);
+  } else {
+    localStorage.removeItem("Donor_id");
+  }
 
   return result;
 };
