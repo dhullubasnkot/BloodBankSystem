573e60214752516f05065cca73b611ae3c6e8455
dhullubasnkot <123baglung@gmail.com>
2025-06-27 14:00:10 +0545

    changed in App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import OurCollaboration from "./pages/OurCollabration";
+import OurMission from "./pages/ourMission";
 import AppRoutes from "./routes/routes";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />;
+      <OurCollaboration />
+      <OurMission />
+    </>
+  );
 }
 
 export default App;
