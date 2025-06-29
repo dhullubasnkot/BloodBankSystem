2d8ebff6e24168a4cffcb2880a23f1be91cc0041
dhullubasnkot <123baglung@gmail.com>
2025-06-29 14:19:03 +0545

    update(DonorRoutes.ts): changes made in DonorRoutes.ts

--- Backend/src/routes/DonorRoutes.ts
+++ Backend/src/routes/DonorRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   CreateDonorController,
   GetAllListedBloodDonorsController,
+  GetDonorById,
   SearchDonorController,
 } from "../controller/DonorController";
 import { authenticateToken } from "../middleware/auth";
@@ -10,5 +11,6 @@ const router = express.Router();
 
 router.post("/", CreateDonorController);
 router.get("/search", SearchDonorController);
-router.get("/",authenticateToken, GetAllListedBloodDonorsController);
+router.get("/", authenticateToken, GetAllListedBloodDonorsController);
+router.get("/:id", GetDonorById);
 export default router;
